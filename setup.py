e9d33999a5c4ac70097a60efba3c344aabe15c45
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-03-16 18:49:48 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
   name='conda_minifier',
-  author="Christian Moscardi, Tianhui Michael Li"
+  author="Christian Moscardi, Tianhui Michael Li",
   version='0.1',
   py_modules=['conda_minifier'],
 )
